2d79d80b75000718f21315845887175dcaa14d6a
RadithSandeepa <radithsandeepa500@gmail.com>
2026-05-15 13:17:04 +0530

    fix: Fix privateKey formatting in Firebase config

--- config/firebase.js
+++ config/firebase.js
@@ -17,7 +17,7 @@ if (!FIREBASE_PROJECT_ID || !FIREBASE_CLIENT_EMAIL || !FIREBASE_PRIVATE_KEY || !
 const serviceAccount = {
   projectId: FIREBASE_PROJECT_ID,
   clientEmail: FIREBASE_CLIENT_EMAIL,
-  privateKey: FIREBASE_PRIVATE_KEY,
+  privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
 };
 
 admin.initializeApp({
